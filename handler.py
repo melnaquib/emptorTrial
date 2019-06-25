029016229f39ec56866cf1464d13e35ae9b7be76
Mustafa Elnaquib <melnaquib@gmail.com>
2019-06-25 23:57:17 +0200

    Second Version saves url content to s3 and title to dynamob

--- handler.py
+++ handler.py
@@ -4,6 +4,18 @@ import urllib
 from bs4 import BeautifulSoup
 import traceback
 
+import boto3
+
+
+cfg = {
+    's3': {
+        'bucket': "emptortrial-content",
+    },
+    'dyndb': {
+        'table': 'emptorTrial_titles'
+    }
+}
+
 
 def title(event, context):
     tb = ''
@@ -11,18 +23,29 @@ def title(event, context):
 
         url = event['queryStringParameters']['url']
         url = urllib.parse.unquote(url)
+        key = str(hash(url))
 
         rsp = requests.get(url)
         soup = BeautifulSoup(rsp.content, 'html.parser')
-        title = soup.title.string
+        doctitle = soup.title.string
+
+        s3 = boto3.resource('s3')
+        bucket = s3.Bucket(cfg['s3']['bucket'])
+
+        dynamodb = boto3.resource('dynamodb')
+        table = dynamodb.Table(cfg['dyndb']['table'])
+
+        table.put_item(Item={'id': key, 'title': doctitle})
+        bucket.put_object(cfg['s3']['bucket'], Key=key, Body=rsp.content)
 
         return {
             'statusCode': 200,
-            'body': json.dumps({"title": title}),
+            'body': json.dumps({"title": doctitle}),
             'headers': {
                 'Content-Type': 'application/json',
             }
         }
+
 
     #TODO; more info in error handling
     except Exception as ex:
