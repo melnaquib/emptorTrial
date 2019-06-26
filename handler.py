fc5f252f7ab320d9008891e206eafeb3cee2cfb9
Mustafa Elnaquib <melnaquib@gmail.com>
2019-06-26 07:49:08 +0200

    Fourth Version call using dynamo streams

--- handler.py
+++ handler.py
@@ -3,9 +3,13 @@ from botocore.vendored import requests
 import urllib
 from bs4 import BeautifulSoup
 import traceback
-
 import boto3
 import asyncio
+
+import logging
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
 
 cfg = {
     's3': {
@@ -17,41 +21,40 @@ cfg = {
 }
 
 
-async def title(key):
-    tb = ''
-    try:
-        dynamodb = boto3.resource('dynamodb')
-        table = dynamodb.Table(cfg['dyndb']['table'])
-        res = table.get_item( Key={'id': key})
-        item = res['Item']
-        url = item['url']
+def title_process(bucket, table, key, url):
+    logger.info("BGN " + key)
+    rsp = requests.get(url)
+    soup = BeautifulSoup(rsp.content, 'html.parser')
+    doctitle = soup.title.string
+    # doctitle = str(hash(rsp.content))
 
-        rsp = requests.get(url)
-        soup = BeautifulSoup(rsp.content, 'html.parser')
-        doctitle = soup.title.string
+    bucket.put_object(bucket.name, Key=key, Body=rsp.content)
+    s3url = 'https://{bucket}.s3.amazonaws.com/{key}'.format(bucket=cfg['s3']['bucket'], key=key)
 
-        s3 = boto3.resource('s3')
-        bucket = s3.Bucket(cfg['s3']['bucket'])
-        bucket.put_object(cfg['s3']['bucket'], Key=key, Body=rsp.content)
-
-        s3url = 'https://{bucket}.s3.amazonaws.com/{key}'.format(bucket=cfg['s3']['bucket'], key=key)
-
-        dynamodb = boto3.resource('dynamodb')
-        table = dynamodb.Table(cfg['dyndb']['table'])
-        table.update_item(Key={'id': key}, AttributeUpdates={'s3url': s3url, 'title': doctitle, 'status': 'PROCESSED'})
+    return table.update_item(Key={'id': key}, AttributeUpdates={'s3url': {'Value': s3url}, 'title': {'Value': doctitle},
+                                                                'status': {'Value': 'PROCESSED'}})
 
 
-    #TODO; more info in error handling
-    except Exception as ex:
-        tb = traceback.format_exc()
+def title(event, context):
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table(cfg['dyndb']['table'])
 
-    return {
-        'statusCode': 200,
-        'body': {"error": tb},
-        'headers': {
-            'Content-Type': 'application/json',
-        }
-    }
+    s3 = boto3.resource('s3')
+    bucket = s3.Bucket(cfg['s3']['bucket'])
+
+    # TODO, async
+    for record in event['Records']:
+        try:
+            if 'INSERT' != record['eventName'] or 'PENDING' != record['dynamodb']['NewImage']['status']:
+                logger.info("SKIPPED " + json.dumps(record))
+                continue
+
+            item = record['dynamodb']['NewImage']
+            res = title_process(bucket, table, list(item['Id'].values())[0], item['url'])
+            logger.info(res)
+
+        except Exception as ex:
+            logger.exception(ex)
 
 
 def title_submit(event, context):
@@ -66,8 +69,6 @@ def title_submit(event, context):
         table = dynamodb.Table(cfg['dyndb']['table'])
 
         table.put_item(Item={'id': key, 'url': url, 'status': 'PENDING'})
-
-        asyncio.run(title(key))
 
         return {
             'statusCode': 200,
